be9850aa356b484240bf5802cf13440399875983
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-04-02 14:11:23 +0300

    hamming

---
+++ hamming/src/hamming.clj
@@ -0,0 +1,11 @@
+(ns hamming)
+
+(defn distance
+  "Calculate the Hamming Distance between two DNA strands"
+  [strand1 strand2]
+  (when (= (count strand1) (count strand2))
+    (reduce (fn [result x]
+              (if (false? x)
+                (inc result)
+                result))
+            0 (map = strand1 strand2))))
