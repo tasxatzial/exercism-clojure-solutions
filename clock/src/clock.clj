5d36d7a7937bc863d05e49dfb93fee58bdf0c677
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-06-16 22:16:33 +0300

    clock: refactoring

--- clock/src/clock.clj
+++ clock/src/clock.clj
@@ -1,26 +1,33 @@
 (ns clock)
 
+(defn prefix-zero
+  "Prefixes n with 0 iff n < 10 and returns the corresponding string."
+  [n]
+  (if (< n 10)
+    (str "0" n)
+    (str n)))
+
 (defn clock->string
-  "Adds a zero in front of a 1-digit number"
+  "Prefixes hour and/or minutes with a 0 (if they are < 10) and
+  returns the string hour:minutes"
   [[hour minutes]]
-  (letfn [(add-zero [num]
-            (if (< num 10)
-              (str "0" num)
-              (str num)))]
-    (str (add-zero hour) ":" (add-zero minutes)))
-)
+  (str (prefix-zero hour) ":" (prefix-zero minutes)))
+
+(defn min->hour
+  "Calculates how many hours should be added or subtracted
+  from an integer number of hours. E.g. 70 mins add 1 hour
+  to 5 hours (6h+10min), -10 mins subtract -1 hour (4h+50min)"
+  [^Integer minutes]
+  (Math/floorDiv minutes 60))
 
 (defn clock
-  "Converts hours/minutes to a 24h/60m clock"
+  "Converts hours/minutes to 24h time."
   [hours minutes]
-  (let [minutes_q (if (< minutes 0)
-                       (dec (quot minutes 60))
-                       (quot minutes 60))]
-    [(mod (+ minutes_q hours) 24) (mod minutes 60)])
-)
+  (let [extra-hours (min->hour minutes)]
+    [(mod (+ extra-hours hours) 24)
+     (mod minutes 60)]))
 
 (defn add-time
-  "Adds minutes to a clock"
-  [clock_ minutes]
-  (clock (clock_ 0) (+ (clock_ 1) minutes))
-)
+  "Adds minutes to a 24h time."
+  [[hours minutes] extra-minutes]
+  (clock hours (+ minutes extra-minutes)))
