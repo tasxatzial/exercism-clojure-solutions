b105b9a408add50f6e18a5ec0ccb1e03b20f35bd
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-06-16 17:41:20 +0300

    collatz-conjecture: refactoring

--- collatz-conjecture/src/collatz_conjecture.clj
+++ collatz-conjecture/src/collatz_conjecture.clj
@@ -1,16 +1,17 @@
 (ns collatz-conjecture)
 
 (defn collatz
-  "Return the number of iterations until the specified number equals to 1 according
-  to the algorithm described in the Collatz conjecture.
-
-  The specified number must be a positive integer."
-  [num]
-  (if (or (not (integer? num)) (<= num 0))
-    (throw (Exception. "Invalid number."))
-    (loop [number num steps 0]
-      (if (= number 1)
+  "Applies the collatz conjecture algorithms and returns the number of iterations
+  until the specified number equals to 1."
+  [n]
+  (if (pos-int? n)
+    (loop [num n
+           steps 0]
+      (if (= num 1)
         steps
-        (if (= (mod number 2) 0)
-          (recur (/ number 2) (inc steps))
-          (recur ((comp inc *) number 3) (inc steps)))))))
+        (if (= (mod num 2) 0)
+          (recur (/ num 2)
+                 (inc steps))
+          (recur ((comp inc *) num 3)
+                 (inc steps)))))
+    (throw (Exception.))))
