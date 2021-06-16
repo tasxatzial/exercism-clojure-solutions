ea1f20381cc02637c51a0bc4ba581eb0a5607f7a
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-06-16 17:28:07 +0300

    armstrong-numbers: refactoring

--- armstrong-numbers/src/armstrong_numbers.clj
+++ armstrong-numbers/src/armstrong_numbers.clj
@@ -1,5 +1,29 @@
 (ns armstrong-numbers)
 
-(defn armstrong? [num]
-  (def s (->> num str (map (comp read-string str))))
-  (= num (apply + (map #(reduce * (repeat (count s) %)) s))))
+(defn int->digits
+  "Converts an integer to a seq of its digits."
+  [s]
+  (map #(Integer/parseInt %)
+       (clojure.string/split (str s) #"")))
+
+(defn math-pow
+  "Calculates x^n."
+  [x n]
+  (loop [result 1
+         remaining n]
+    (if (= 0 remaining)
+      result
+      (recur (* x result)
+             (dec remaining)))))
+
+(defn armstrong?
+  "Determines whether a number is an Armstrong number."
+  [n]
+  (let [digits (int->digits n)
+        digit-count (count digits)]
+    (loop [sum 0
+           [first-digit & remaining-digits] digits]
+      (if (boolean first-digit)
+        (recur (+ sum (math-pow first-digit digit-count))
+               remaining-digits)
+        (= sum n)))))
