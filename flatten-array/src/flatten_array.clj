5f113d063c519e8b23ecf8b31bd3fd8f55a6c9e8
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-06-16 22:44:51 +0300

    flatten-array: refactoring

--- flatten-array/src/flatten_array.clj
+++ flatten-array/src/flatten_array.clj
@@ -1,20 +1,26 @@
 (ns flatten-array)
 
-;;using reduce
-(defn flatten
-  "Flattens a sequence and removes any nil values"
-  [arr]
-  (let [flattened (reduce (fn [result x]
-                            (if (sequential? x)
-                              (concat result (flatten x))
-                              (concat result [x])))
-                          '() arr)]
-    (filter (complement nil?) flattened))
-)
+(defn flatten-no-removal
+  "Flattens a sequential struct. Does not remove nil values."
+  [coll]
+  (reduce (fn [result x]
+            (into result
+                  (if (sequential? x)
+                    (flatten-no-removal x)
+                    [x])))
+          []
+          coll))
 
-;;using clojure.core/flatten
+;; using flatten-no-removal
+(defn flatten
+  "Flattens a sequential struct and removes any nil values."
+  [coll]
+  (->> (flatten-no-removal coll)
+       (filter (complement nil?))))
+
+;; using clojure.core/flatten
 (defn flatten2
-  "Flattens a sequence and removes any nil values"
-  [arr]
-  (filter (complement nil?) (clojure.core/flatten arr))
-)
+  "Flattens a sequential struct and removes any nil values."
+  [coll]
+  (->> (clojure.core/flatten coll)
+       (filter (complement nil?))))
