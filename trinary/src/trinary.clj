93f1f193ee8151e1b1d1389432f0bcf4aa44299c
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-06-19 02:18:00 +0300

    trinary

---
+++ trinary/src/trinary.clj
@@ -0,0 +1,32 @@
+(ns trinary)
+
+(defn string->digits
+  "Converts a string to a seq of its digits. Numeric characters
+  are guaranteed to be represented by their numerical value."
+  [s]
+  (map #(- (int %) 48) s))
+
+(defn valid-trinary?
+  "Returns true iff digits seq represents a valid trinary number."
+  [digits]
+  (every? #(<= 0 % 2) digits))
+
+(defn compute-val
+  "Returns the decimal value of a trinary number represented
+  by a seq of its digits."
+  [digits]
+  (+ (reduce (fn [result digit]
+               (* 3 (+ result digit)))
+             0
+             (butlast digits))
+     (last digits)))
+
+(defn to-decimal
+  "Returns the decimal value of the trinary number represented
+  by the given string or 0 if the string does not represent a valid
+  trinary number."
+  [s]
+  (let [digits (string->digits s)]
+    (if (valid-trinary? digits)
+      (compute-val digits)
+      0)))
