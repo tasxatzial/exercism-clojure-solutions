1bd66ff2a8bbbe2e0992217d6e0a97f4d8f32b36
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-06-21 01:56:14 +0300

    largest-series-product: fix failing tests

--- largest-series-product/src/largest_series_product.clj
+++ largest-series-product/src/largest_series_product.clj
@@ -3,21 +3,31 @@
 (defn int->digits
   "Converts an integer to a seq of its digits."
   [s]
-  (map #(Integer/parseInt %)
-       (map str s)))
+  (map #(Character/digit ^char % 10)
+       (str s)))
+
+(defn valid-digits?
+  "Checks if the digits seq represents a valid integer."
+  [digits]
+  (every? #(<= 0 % 9)
+          digits))
 
 (defn largest-product
   "Calculates the largest product for a contiguous substring of digits
   of length n."
   [n s]
   (let [digits (int->digits s)]
-    (if (> n (count digits))
+    (if (or (> n (count digits))
+            (< n 0)
+            (not (valid-digits? digits)))
       (throw (Exception.))
-      (loop [max 0
-             remaining-digits digits]
-        (if (>= (count remaining-digits) n)
-          (let [mult (apply * (take n remaining-digits))]
-            (if (> mult max)
-              (recur mult (rest remaining-digits))
-              (recur max (rest remaining-digits))))
-          max)))))
+      (if (= 0 n)
+        1
+        (loop [max 0
+               remaining-digits digits]
+          (if (>= (count remaining-digits) n)
+            (let [mult (apply * (take n remaining-digits))]
+              (if (> mult max)
+                (recur mult (rest remaining-digits))
+                (recur max (rest remaining-digits))))
+            max))))))
