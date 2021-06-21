ca04b9736a6d76c420096b9318f2e66241c0d948
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-06-21 17:39:00 +0300

    sum-of-multiples

---
+++ sum-of-multiples/src/sum_of_multiples.clj
@@ -0,0 +1,15 @@
+(ns sum-of-multiples)
+
+(defn multiples
+  "Returns all multiples of n which are less than max."
+  [n max]
+  (let [quotient (float (dec (/ max n)))]
+    (take quotient (iterate (partial + n) n))))
+
+(defn sum-of-multiples
+  "Given a list of nums, find the sum of all the unique multiples of
+  the numbers up to but not including the given max number."
+  [nums max]
+  (apply + (reduce #(into %1 (multiples %2 max))
+                   #{}
+                   nums)))
