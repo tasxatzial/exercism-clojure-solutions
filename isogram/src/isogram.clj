9f7506176452ce55720e2f972ea8e1f15ae0ba31
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-06-24 22:43:07 +0300

    isogram

---
+++ isogram/src/isogram.clj
@@ -0,0 +1,21 @@
+(ns isogram)
+
+(defn collect-letters
+  "Collects the letters of lowercase s in a vector."
+  [s]
+  (let [lowercased (clojure.string/lower-case s)]
+    (reduce (fn [result c]
+              (if (or (= \space c)
+                      (= \- c))
+                result
+                (conj result c)))
+            []
+            lowercased)))
+
+(defn isogram?
+  "Returns true if s is isogram, false otherwise."
+  [s]
+  (let [letters (collect-letters s)
+        letters-set (set letters)]
+    (= (count letters)
+       (count letters-set))))
