df6068b82ccea7ae8b0bcf6cc5a2024f0d81a626
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-06-27 14:35:32 +0300

    sieve: avoid using the mod function as per instructions

--- sieve/src/sieve.clj
+++ sieve/src/sieve.clj
@@ -1,13 +1,18 @@
 (ns sieve)
 
+(defn multiples-of
+  "Generates a set of all multiples of n that are <= max
+  and >= n^2."
+  [n max]
+  (set (take-while #(<= % max)
+                   (iterate #(+ % n) (* n n)))))
+
 (defn sieve
-  "Returns all the primes from 2 up to a given number N."
-  [N]
-  (let [numbers (range 2 (inc N))]
-    (loop [result []
-           [num & more] numbers]
-      (if num
-        (recur (conj result num)
-               (filter #(not= 0 (mod % num))
-                       more))
-        result))))
+  "Returns all primes in [2, max]."
+  [max]
+  (loop [primes []
+         [n & remaining] (range 2 (inc max))]
+    (if n
+      (recur (conj primes n)
+             (remove (multiples-of n max) remaining))
+      primes)))
