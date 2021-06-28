18ec5c86d6308985ce072a3761b6eac5c886070c
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-06-28 13:15:23 +0300

    proverb

---
+++ proverb/src/proverb.clj
@@ -0,0 +1,10 @@
+(ns proverb)
+
+(defn recite
+  "Generates the relevant proverb from the given list of words."
+  [s]
+  (if (seq s)
+    (let [first-s (mapv #(str "For want of a " %1 " the " %2 " was lost.") s (rest s))
+          last-s (str "And all for the want of a " (first s) ".")]
+      (clojure.string/join "\n" (conj first-s last-s)))
+    ""))
