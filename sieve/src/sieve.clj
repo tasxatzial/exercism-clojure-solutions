8c48c8b7ee46312b0efbac0bf900f9c5a60a6fec
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-06-29 17:08:04 +0300

    sieve: optimized solution

--- sieve/src/sieve.clj
+++ sieve/src/sieve.clj
@@ -2,20 +2,18 @@
 
 ;; optimized sieve
 
-(defn- multiples
-  "Generates a set of multiples of n that are <=max
-  and >=n^2. Also every second multiple is skipped.
-  Intended to be used when n>2"
-  [n max]
-  (set (range (* n n) (inc max) (+ n n))))
+(defn composites
+  "Returns a lazy seq of composites in [2, max].
+  Even numbers are left out."
+  [max]
+  (for [i (range 3 (inc (Math/round (Math/sqrt max))) 2)
+        k (range i (inc (/ max i)) 2)]
+    (* i k)))
 
 (defn sieve
-  "Returns all primes in [2, max]."
+  "Returns all primes in [2, max]. "
   [max]
   (if (< max 2)
     ()
-    (let [numbers (range 3 (inc max))
-          odd (filter odd? numbers)
-          remove-multiples #(remove (multiples %2 max) %1)
-          primes-gt2 (reduce remove-multiples odd odd)]
-      (cons 2 primes-gt2))))
+    (remove (set (composites max))
+            (cons 2 (range 3 (inc max) 2)))))
