efb0c0cb95ab58b8f8fffb5d03f014f78896d8f3
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-07-01 15:02:23 +0300

    matching-brackets: refactoring

--- matching-brackets/src/matching_brackets.clj
+++ matching-brackets/src/matching_brackets.clj
@@ -1,19 +1,20 @@
 (ns matching-brackets)
 
+;; using a vector as stack
+
 (def brackets #{\{ \} \( \) \[ \]})
-(def matches {\{ \} \( \) \[ \]})
+(def matching-bracket {\{ \} \( \) \[ \]})
 
 (defn valid?
   "Returns true if the given string has properly
   matched brackets, false otherwise."
   [s]
   (loop [stack []
-         s (filter brackets s)]
-    (if (seq s)
-      (let [bracket (first s)]
-        (if (contains? matches bracket)
-          (recur (conj stack bracket) (rest s))
-          (if (= bracket (matches (peek stack)))
-            (recur (pop stack) (rest s))
-            false)))
+         [bracket & rest-brackets] (filter brackets s)]
+    (if bracket
+      (if (matching-bracket bracket)
+        (recur (conj stack bracket) rest-brackets)
+        (if (= bracket (matching-bracket (peek stack)))
+          (recur (pop stack) rest-brackets)
+          false))
       (empty? stack))))
