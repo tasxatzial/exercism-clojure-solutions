a89f23ad51d5c060c46c44ba45e3730e1c6e0262
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-07-04 21:31:38 +0300

    sieve: added faster solution

--- sieve/src/sieve.clj
+++ sieve/src/sieve.clj
@@ -2,6 +2,7 @@
 
 ;; optimized sieve
 
+;; solution 1, (simpler, less demanding)
 (defn composites
   "Returns a lazy seq of composites in [2, max].
   Even numbers are left out."
@@ -11,9 +12,37 @@
     (* i k)))
 
 (defn sieve
-  "Returns all primes in [2, max]. "
+  "Returns all primes in [2, max]."
   [max]
   (if (< max 2)
     ()
     (remove (set (composites max))
             (cons 2 (range 3 (inc max) 2)))))
+
+;; solution 2 (6 times faster, more demanding)
+(defn delete-mult-k
+  "Deletes multiples of i (except i) from nums
+  (replaces them with 0)"
+  [i nums max]
+  (let [lim (Math/round (Math/floor (/ max i)))
+        k (if (even? lim) (dec lim) lim)]
+    (loop [k k
+           nums nums]
+      (if (< k i)
+        nums
+        (if (pos? (get nums (/ (- k 3) 2)))
+          (recur (- k 2) (assoc nums (/ (- (* i k) 3) 2) 0))
+          (recur (- k 2) nums))))))
+
+(defn sieve2
+  "Returns all primes in [2, max]."
+  [max]
+  (let [nums (vec (take (/ max 2) (range 3 (inc max) 2)))
+        sqrt (Math/round (Math/floor (Math/sqrt max)))]
+    (loop [i 3
+           nums nums]
+      (if (> i sqrt)
+        (cons 2 (filter pos? nums))
+        (if (pos? (get nums (/ (- i 3) 2)))
+          (recur (+ i 2) (delete-mult-k i nums max))
+          (recur (+ i 2) nums))))))
