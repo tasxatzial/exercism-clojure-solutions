e94f7d75e510333ab0e7ad504c3351a0e8a95de6
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-07-12 18:43:53 +0300

    collatz-conjecture: refactoring & add second solution

--- collatz-conjecture/src/collatz_conjecture.clj
+++ collatz-conjecture/src/collatz_conjecture.clj
@@ -1,16 +1,29 @@
 (ns collatz-conjecture)
 
+(defn next-num
+  "Calculates the next number in the collatz conjecture
+  algorithm."
+  [n]
+  (if (even? n)
+    (/ n 2)
+    ((comp inc *) n 3)))
+
 (defn collatz
-  "Applies the collatz conjecture algorithms and returns the number of iterations
-  until the specified number equals to 1."
+  "Applies the collatz conjecture algorithm and returns
+  the number of iterations until the specified number
+  equals to 1."
   [n]
   (if (pos-int? n)
     (loop [num n
            steps 0]
       (if (= num 1)
         steps
-        (if (= (mod num 2) 0)
-          (recur (/ num 2) (inc steps))
-          (recur ((comp inc *) num 3)
-                 (inc steps)))))
+        (recur (next-num num) (inc steps))))
+    (throw (Exception.))))
+
+;; solution2: using iterate
+(defn collatz2
+  [n]
+  (if (pos-int? n)
+    (count (take-while #(not= 1 %) (iterate next-num n)))
     (throw (Exception.))))
