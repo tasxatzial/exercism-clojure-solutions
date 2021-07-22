ffa77a4c611ea751d78e452aea6a518c98363a68
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-07-22 19:35:08 +0300

    clock: refactoring

--- clock/src/clock.clj
+++ clock/src/clock.clj
@@ -1,7 +1,7 @@
 (ns clock)
 
 (defn prefix-zero
-  "Prefixes n with 0 iff n < 10 and returns the corresponding string."
+  "Prefixes n with 0 if n < 10 and returns the corresponding string."
   [n]
   (if (< n 10)
     (str "0" n)
@@ -13,17 +13,10 @@
   [[hour minutes]]
   (str (prefix-zero hour) ":" (prefix-zero minutes)))
 
-(defn min->hour
-  "Calculates how many hours should be added or subtracted
-  from an integer number of hours. E.g. 70 mins add 1 hour
-  to 5 hours (6h+10min), -10 mins subtract -1 hour (4h+50min)"
-  [^Integer minutes]
-  (Math/floorDiv minutes 60))
-
 (defn clock
   "Converts hours/minutes to 24h time."
   [hours minutes]
-  (let [extra-hours (min->hour minutes)]
+  (let [extra-hours (Math/floorDiv ^Integer minutes 60)]
     [(mod (+ extra-hours hours) 24)
      (mod minutes 60)]))
 
