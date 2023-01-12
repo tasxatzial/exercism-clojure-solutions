88dff530a0d6248c1a5a63324b4477d3738c17d8
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-12 20:19:28 +0200

    hamming: update exercise & refactoring solution

--- hamming/src/hamming.clj
+++ hamming/src/hamming.clj
@@ -1,12 +1,8 @@
 (ns hamming)
 
 (defn distance
-  "Calculate the Hamming Distance between two DNA strands"
   [strand1 strand2]
   (when (= (count strand1) (count strand2))
-    (reduce (fn [result x]
-              (if (false? x)
-                (inc result)
-                result))
-            0
-            (map = strand1 strand2))))
+    (->> (map = strand1 strand2)
+         (filter false?)
+         count)))
