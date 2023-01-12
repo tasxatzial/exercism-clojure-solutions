949cd3a9f5fcb1ea0c66c661820abb368328fa19
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-12 18:01:23 +0200

    accumulate: update exercise & refactoring solution

--- accumulate/src/accumulate.clj
+++ accumulate/src/accumulate.clj
@@ -1,14 +1,10 @@
 (ns accumulate)
 
 (defn accumulate
-  "Given a collection and an operation to perform on
-  each element of the collection, returns a new collection
-  containing the result of applying that operation to each
-  element of the input collection."
   [f coll]
   (loop [result []
-         [item & more] coll]
-    (if item
-      (recur (conj result (f item))
-             more)
+         coll coll]
+    (if (seq coll)
+      (let [fx (f (first coll))]
+        (recur (conj result fx) (rest coll)))
       result)))
