0f4d62b510a8a4680b1374982f9f6530e98aced0
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-12 21:58:01 +0200

    protein translation: update exercise & refactoring solution

--- protein-translation/src/protein_translation.clj
+++ protein-translation/src/protein_translation.clj
@@ -1,6 +1,6 @@
 (ns protein-translation)
 
-(def condon->protein
+(def codon->protein
   {"AUG" "Methionine"
    "UUU" "Phenylalanine"
    "UUC" "Phenylalanine"
@@ -19,20 +19,20 @@
    "UGA" "STOP"
    "UAG" "STOP"})
 
+(defn partition-into-codons
+  "Partitions the given string into a seq of codons."
+  [s]
+  (->> s
+       (partition 3)
+       (map #(apply str %))))
+
 (defn translate-codon
-  "Translates a condon to protein name or STOP."
-  [condon]
-  (get condon->protein condon))
+  [s]
+  (get codon->protein s))
 
 (defn translate-rna
-  "Translates a rna string to a protein name sequence."
   [s]
-  (let [condons (partition 3 s)]
-    (loop [result []
-           [condon & more] condons]
-      (if condon
-        (let [protein (get condon->protein (apply str condon))]
-          (if (= "STOP" protein)
-            result
-            (recur (conj result protein) more)))
-        result))))
+  (->> s
+       partition-into-codons
+       (map codon->protein)
+       (take-while #(not= "STOP" %))))
