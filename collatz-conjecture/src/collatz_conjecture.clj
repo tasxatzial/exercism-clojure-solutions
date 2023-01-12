2a5e9741eea643b48db960e094bfeb63e0983935
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-12 19:31:29 +0200

    collatz-conjecture: update exercise & refactoring solution & remove slowest solution

--- collatz-conjecture/src/collatz_conjecture.clj
+++ collatz-conjecture/src/collatz_conjecture.clj
@@ -1,29 +1,18 @@
 (ns collatz-conjecture)
 
-(defn next-num
-  "Calculates the next number in the collatz conjecture
-  algorithm."
+(defn collatz-next
+  "Calculates the next number in the collatz conjecture algorithm."
   [n]
   (if (even? n)
     (/ n 2)
     ((comp inc *) n 3)))
 
 (defn collatz
-  "Applies the collatz conjecture algorithm and returns
-  the number of iterations until the specified number
-  equals to 1."
   [n]
   (if (pos-int? n)
-    (loop [num n
+    (loop [n n
            steps 0]
-      (if (= num 1)
+      (if (= n 1)
         steps
-        (recur (next-num num) (inc steps))))
-    (throw (Exception.))))
-
-;; solution2: using iterate
-(defn collatz2
-  [n]
-  (if (pos-int? n)
-    (count (take-while #(not= 1 %) (iterate next-num n)))
+        (recur (collatz-next n) (inc steps))))
     (throw (Exception.))))
