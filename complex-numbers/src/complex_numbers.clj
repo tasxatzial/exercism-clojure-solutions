4414719eedbf9de9906dcb3cda906dada90785bc
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-12 19:38:32 +0200

    complex numbers

---
+++ complex-numbers/src/complex_numbers.clj
@@ -0,0 +1,44 @@
+(ns complex-numbers)
+
+(defn real
+  "Returns the real part of a complex number a + bI."
+  [[a _]]
+  a)
+
+(defn imaginary
+  "Returns the imaginary part of a complex number a + bI."
+  [[_ b]]
+  b)
+
+(defn abs
+  "Returns the absolute value of a complex number a + bI."
+  [[a b]]
+  (Math/sqrt (+ (* a a) (* b b))))
+
+(defn conjugate
+  "Returns the conjugate of a complex number a + bI."
+  [[a b]]
+  [a (- 0 b)])
+
+(defn add
+  "Adds a + bI to c + dI."
+  [[a b] [c d]]
+  [(+ a c) (+ b d)])
+
+(defn sub
+  "Subtracts c + dI from a + bI."
+  [[a b] [c d]]
+  [(- a c) (- b d)])
+
+(defn mul
+  "Multiples a + bI by c + dI."
+  [[a b] [c d]]
+  [(- (* a c) (* b d))
+   (+ (* b c) (* a d))])
+
+(defn div
+  "Divides a + bI by c + dI."
+  [[a b] [c d]]
+  (let [denom (+ (* c c) (* d d))]
+    [(float (/ (+ (* a c) (* b d)) denom))
+     (float (/ (- (* b c) (* a d)) denom))]))
