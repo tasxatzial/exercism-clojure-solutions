8916c51db5cc5c53f8883ab9af8abd49328ea89c
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-12 20:08:35 +0200

    etl: update exercise & refactoring solution

--- etl/src/etl.clj
+++ etl/src/etl.clj
@@ -1,9 +1,11 @@
 (ns etl)
 
 (defn transform
-  "Transform the legacy data to the shiny new format."
   [source]
   (reduce (fn [result [score words]]
-            (let [lower-words (map clojure.string/lower-case words)]
-              (into result (mapv #(vector % score) lower-words))))
-          {} source))
+            (->> words
+                 (map clojure.string/lower-case)
+                 (mapv #(vector % score))
+                 (into result)))
+          {}
+          source))
