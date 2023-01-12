c16f4c06617184a3ba29cce8026b03bcd3c60694
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-12 18:02:48 +0200

    accumulate: add lazy solution

--- accumulate/src/accumulate.clj
+++ accumulate/src/accumulate.clj
@@ -8,3 +8,10 @@
       (let [fx (f (first coll))]
         (recur (conj result fx) (rest coll)))
       result)))
+
+;; lazy
+(defn accumulate
+  [f coll]
+  (lazy-seq
+    (when (seq coll)
+      (cons (f (first coll)) (accumulate f (rest coll))))))
