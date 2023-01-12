6ee6087b185ce551597cbf15023ace7cac9525cb
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-12 20:55:37 +0200

    pangram: update exercise & refactoring solution

--- pangram/src/pangram.clj
+++ pangram/src/pangram.clj
@@ -1,10 +1,22 @@
 (ns pangram)
 
-(defn pangram?
-  "Returns true if s is a pangram, false otherwise."
+(def alphabet-letter-count 26)
+
+(defn lower-case-letter?
+  "Returns true if character c is a lower-case letter."
+  [c]
+  (<= 97 (int c) 122))
+
+(defn count-different-letters
+  "Returns the number of different letters contained in the given
+  string (ignores case)."
   [s]
-  (= 26 (->> s
-             clojure.string/lower-case
-             (filter #(<= 97 (int %) 122))
-             set
-             count)))
+  (->> s
+       clojure.string/lower-case
+       (filter lower-case-letter?)
+       set
+       count))
+
+(defn pangram?
+  [s]
+  (= alphabet-letter-count (count-different-letters s)))
