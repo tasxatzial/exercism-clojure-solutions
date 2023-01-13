6926ce59333e41e12e9a2feae86a31d2ba5668eb
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-13 17:41:51 +0200

    series: update exercise & refactoring solution

--- series/src/series.clj
+++ series/src/series.clj
@@ -1,15 +1,14 @@
 (ns series)
 
 (defn slices
-  "Return all the contiguous substrings of length n in
-   a string in the order that they appear."
   [string length]
-  (if (= 0 length)
+  (if (zero? length)
     [""]
-    (loop [result []
-           digits (seq string)]
-      (let [slice (take length digits)]
-        (if (< (count slice) length)
-          result
-          (recur (conj result (apply str slice))
-                 (rest digits)))))))
+    (let [last-index (inc (- (count string) length))]
+      (loop [result []
+             index 0
+             string string]
+        (if (< index last-index)
+          (let [slice (apply str (take length string))]
+            (recur (conj result slice) (inc index) (rest string)))
+          result)))))
