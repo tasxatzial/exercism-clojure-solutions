58e15cb77abc273e69dc24370dfb4136f8cf2577
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-13 21:38:32 +0200

    sum of multiples: update exercise & refactoring solution

--- sum-of-multiples/src/sum_of_multiples.clj
+++ sum-of-multiples/src/sum_of_multiples.clj
@@ -1,13 +1,13 @@
 (ns sum-of-multiples)
 
-(defn multiples
+(defn get-multiples
   "Returns all multiples of n which are less than max."
   [n max]
   (range n max n))
 
 (defn sum-of-multiples
-  "Given a list of nums, find the sum of all the unique multiples of
-  the numbers up to but not including the given max number."
   [nums max]
-  (apply + (reduce #(into %1 (multiples %2 max))
-                   #{} nums)))
+  (->> nums
+       (map #(get-multiples % max))
+       (reduce into #{})
+       (apply +)))
