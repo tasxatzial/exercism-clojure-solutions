6d595ede59716317e080c460aa0372399e223ddb
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-14 00:48:19 +0200

    binary search: update exercise & refactoring solution & implement middle function

--- binary-search/src/binary_search.clj
+++ binary-search/src/binary_search.clj
@@ -1,21 +1,18 @@
 (ns binary-search)
 
 (defn search-for
-  "Finds the position of n in a sorted coll. Throws exception
-  when n is not found."
-  ([n coll] (search-for n coll 0 (dec (count coll))))
-  ([n coll low high]
-   (if (> low high)
+  ([n coll]
+   (search-for n (vec coll) 0 (dec (count coll))))
+  ([n coll low-index high-index]
+   (if (> low-index high-index)
      (throw (Exception. "not found"))
-     (let [mid (Math/round (Math/floor (/ (+ high low) 2)))
-           mid-item (nth coll mid)]
+     (let [mid-index (Math/round (Math/floor (/ (+ high-index low-index) 2)))
+           mid-item (get coll mid-index)]
        (cond
-         (= n mid-item) mid
-         (> mid-item n) (recur n coll low (dec mid))
-         :else (recur n coll (inc mid) high))))))
+         (= n mid-item) mid-index
+         (> mid-item n) (recur n coll low-index (dec mid-index))
+         :else (recur n coll (inc mid-index) high-index))))))
 
-(defn middle [coll]
-  ;; not implemented since the tests do not cover the case
-  ;; where coll has even number of items (there is no middle
-  ;; item)
-  )
+(defn middle
+  [coll]
+  (Math/round (Math/floor (/ (count coll) 2))))
