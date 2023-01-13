4a052d75499e177b38311c7b51fd2ed37769cd96
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-13 22:08:40 +0200

    triangle: update exercise

--- triangle/src/triangle.clj
+++ triangle/src/triangle.clj
@@ -1,32 +1,31 @@
 (ns triangle)
 
-(defn is-valid?
+(defn valid-triangle?
   "Returns true if a triangle with sides n1 n2 n3 is valid,
   false otherwise."
   [n1 n2 n3]
-  (and (pos? n1) (pos? n2) (pos? n3)
+  (and (pos? n1)
+       (pos? n2)
+       (pos? n3)
        (> (+ n1 n2) n3)
        (> (+ n1 n3) n2)
        (> (+ n2 n3) n1)))
 
 (defn equilateral?
-  "Returns true if a triangle with sides n1 n2 n3 is equilateral,
-  false otherwise."
   [n1 n2 n3]
-  (and (is-valid? n1 n2 n3) (= n1 n2 n3)))
+  (and (valid-triangle? n1 n2 n3)
+       (= n1 n2 n3)))
 
 (defn isosceles?
-  "Returns true if a triangle with sides n1 n2 n3 is isosceles,
-  false otherwise."
   [n1 n2 n3]
-  (and (is-valid? n1 n2 n3)
-       (or (= n1 n2) (= n1 n3) (= n2 n3))))
+  (and (valid-triangle? n1 n2 n3)
+       (or (= n1 n2)
+           (= n1 n3)
+           (= n2 n3))))
 
 (defn scalene?
-  "Returns true if a triangle with sides n1 n2 n3 is scalene,
-  false otherwise."
   [n1 n2 n3]
-  (and (is-valid? n1 n2 n3)
+  (and (valid-triangle? n1 n2 n3)
        (not= n1 n2)
        (not= n1 n3)
        (not= n2 n3)))
