2637bd92d42d0da91be9e3947b37f345f6aba2b5
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-13 14:11:01 +0200

    rotational cipher: update exercise & refactoring solution

--- rotational-cipher/src/rotational_cipher.clj
+++ rotational-cipher/src/rotational_cipher.clj
@@ -1,15 +1,38 @@
 (ns rotational-cipher)
 
+(defn get-char-type
+  "Returns:
+  1) :uppercase if int-char represents an uppercase letter
+  2) :lowercase if int-char represents a lowercase letter
+  3) :other in all other cases."
+  [int-char]
+  (cond
+    (<= 97 int-char 122) :uppercase
+    (<= 65 int-char 90) :lowercase
+    :else :other))
+
+(defn rotate-int
+  "Rotates num by the given amount. Num should satisfy
+  lowest-int <= num <= lowest-int + cycle-length."
+  [lowest-int cycle-length num amount]
+  (+ lowest-int (mod (+ amount (- num lowest-int)) cycle-length)))
+
+;; rotate functions based on char type
+(def rotate-fn
+  {:lowercase (partial rotate-int 65 26)
+   :uppercase (partial rotate-int 97 26)})
+
 (defn rotate-char
-  "Shifts character c n times."
+  "Rotates the given char by n."
   [c n]
-  (let [int-c (int c)]
-    (char (cond
-            (<= 65 int-c 90) (+ 65 (mod (+ n (- int-c 65)) 26))
-            (<= 97 int-c 122) (+ 97 (mod (+ n (- int-c 97)) 26))
-            :else c))))
+  (let [int-char (int c)
+        char-type (get-char-type int-char)]
+    (if-let [rotate-fn (rotate-fn char-type)]
+      (char (rotate-fn int-char n))
+      c)))
 
 (defn rotate
-  "Shifts every letter character in s n times."
   [s n]
-  (apply str (map #(rotate-char % n) s)))
+  (->> s
+       (map #(rotate-char % n))
+       (apply str)))
