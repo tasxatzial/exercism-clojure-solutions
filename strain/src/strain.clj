21eea5dbca8d8c3554bb2845c87ad376091bd49f
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-13 18:42:26 +0200

    strain: add lazy implementations

--- strain/src/strain.clj
+++ strain/src/strain.clj
@@ -21,3 +21,23 @@
           (recur result rest-items)
           (recur (conj result item) rest-items)))
       result)))
+
+;; lazy retain
+(defn retain-lazy
+  [f coll]
+  (lazy-seq
+    (when (seq coll)
+      (let [[item & rest-items] coll]
+        (if (true? (f item))
+          (cons item (retain-lazy f rest-items))
+          (retain-lazy f rest-items))))))
+
+;; lazy discard
+(defn discard-lazy
+  [f coll]
+  (lazy-seq
+    (when (seq coll)
+      (let [[item & rest-items] coll]
+        (if (true? (f item))
+          (discard-lazy f rest-items)
+          (cons item (discard-lazy f rest-items)))))))
