4a63193e6f79ef648ebd4af0320bb956f15729f7
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-14 01:10:04 +0200

    flatten array: update exercise & remove old solutions and add new one

--- flatten-array/src/flatten_array.clj
+++ flatten-array/src/flatten_array.clj
@@ -1,24 +1,12 @@
 (ns flatten-array)
 
-(defn flatten-no-removal
-  "Flattens a sequential struct. Does not remove nil values."
-  [coll]
-  (reduce (fn [result x]
-            (into result (if (sequential? x)
-                           (flatten-no-removal x)
-                           [x])))
-          [] coll))
-
-;; using flatten-no-removal
 (defn flatten
-  "Flattens a sequential struct and removes any nil values."
-  [coll]
-  (->> (flatten-no-removal coll)
-       (filter (complement nil?))))
-
-;; using clojure.core/flatten
-(defn flatten2
-  "Flattens a sequential struct and removes any nil values."
-  [coll]
-  (->> (clojure.core/flatten coll)
-       (filter (complement nil?))))
+  [xs]
+  (reduce (fn flat- [result x]
+            (if (sequential? x)
+              (reduce flat- result x)
+              (if (nil? x)
+                result
+                (conj result x))))
+          []
+          xs))
