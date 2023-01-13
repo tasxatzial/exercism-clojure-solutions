6d78b7884eb6819618aa81925f73b19f242c5d67
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-14 01:13:29 +0200

    flatten array: add lazy implementation

--- flatten-array/src/flatten_array.clj
+++ flatten-array/src/flatten_array.clj
@@ -10,3 +10,15 @@
                 (conj result x))))
           []
           xs))
+
+;; lazy
+(defn flatten-lazy
+  [xs]
+  (lazy-seq
+    (when (seq xs)
+      (let [[x & rest-xs] xs]
+        (if (sequential? x)
+          (concat (flatten-lazy x) (flatten-lazy rest-xs))
+          (if (nil? x)
+            (flatten-lazy rest-xs)
+            (cons x (flatten-lazy rest-xs))))))))
