11bdd9b9d3c801a20ca42545cf9d4021d79ab1dd
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-13 21:24:10 +0200

    sublist: update exercise & refactoring solution

--- sublist/src/sublist.clj
+++ sublist/src/sublist.clj
@@ -1,36 +1,20 @@
 (ns sublist)
 
-(defn check-sublist
-  "Returns :sublist if list1 is a sublist of list2, else :unequal."
+(defn sublist?
+  "Returns true of list2 is a sublist of list1, false otherwise."
   [list1 list2]
-  (let [l1-count (count list1)
-        l2-sublists (partition l1-count 1 list2)]
-    (if (some #(= list1 %) l2-sublists)
-      :sublist
-      :unequal)))
-
-(defn check-equal
-  "Returns :equal if list1 = list2, else :unequal."
-  [list1 list2]
-  (if (= list1 list2)
-    :equal
-    :unequal))
-
-(defn check-superlist
-  "Returns :superlist if list1 is superlist of list2, else :unequal."
-  [list1 list2]
-  (if (= :sublist (check-sublist list2 list1))
-    :superlist
-    :unequal))
+  (let [partitioned-list1 (partition (count list2) 1 list1)]
+    (or (some #(= list2 %) partitioned-list1)
+        false)))
 
 (defn classify
-  "Returns
-  :equal if list1 = list2
-  :sublist if list1 is sublist of list2
-  :superlist if list1 is superlist of list2
-  else :unequal"
   [list1 list2]
-  (cond
-    (= (count list1) (count list2)) (check-equal list1 list2)
-    (> (count list1) (count list2)) (check-superlist list1 list2)
-    :else (check-sublist list1 list2)))
+  (if (and (empty? list1) (empty? list2))
+    :equal
+    (if (sublist? list2 list1)
+      (if (= (count list1) (count list2))
+        :equal
+        :sublist)
+      (if (sublist? list1 list2)
+        :superlist
+        :unequal))))
