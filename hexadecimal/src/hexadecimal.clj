2901e8c659280ca584a621dd98be5b5abaafe70d
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-14 21:14:07 +0200

    hexadecimal: update exercise & refactoring solution

--- hexadecimal/src/hexadecimal.clj
+++ hexadecimal/src/hexadecimal.clj
@@ -1,6 +1,6 @@
 (ns hexadecimal)
 
-(defn hex-value
+(defn hex-char->dec
   "Returns the decimal value of a lowercase hexadecimal char."
   [c]
   (let [val (int c)]
@@ -8,32 +8,33 @@
       (- val 48)
       (+ 10 (- val 97)))))
 
-(defn string->digit-values
-  "Converts a string representing a hexadecimal number to a seq of
-  the decimal values of its chars."
+(defn hex->dec-values
+  "Converts a string that represents a non-negative hexadecimal
+  number to a seq of the decimal values of its digits."
   [s]
-  (map hex-value s))
+  (map hex-char->dec s))
 
-(defn valid-hexadecimal?
-  "Returns true iff digits seq represents a valid hexadecimal number."
-  [digits]
-  (every? #(<= 0 % 15) digits))
+(defn valid-hex?
+  "Returns true if the seq of decimal values represents a valid
+  hexadecimal number, false otherwise."
+  [decimal-values]
+  (every? #(<= 0 % 15) decimal-values))
 
-(defn compute-val
-  "Returns the decimal value of a hexadecimal number. The number is
-  represented by the seq of its decimal digit values."
-  [digits]
-  (+ (reduce (fn [result digit]
+(defn compute-decimal
+  "Given a seq of decimal values that represent a hexadecimal
+  number, it computes the decimal equivalent of that number."
+  [decimal-values]
+  (+ (last decimal-values)
+     (reduce (fn [result digit]
                (* 16 (+ result digit)))
-             0 (butlast digits))
-     (last digits)))
+             0
+             (butlast decimal-values))))
 
 (defn hex-to-int
-  "Returns the decimal value of the hexadecimal number represented
-  by the given string or 0 if the string does not represent a valid
-  hexadecimal number."
+  "Converts the given hex number (string) to the decimal equivalent.
+  Returns 0 if the string does not represent a valid hex number."
   [s]
-  (let [digits (string->digit-values s)]
-    (if (valid-hexadecimal? digits)
-      (compute-val digits)
+  (let [dec-values (hex->dec-values s)]
+    (if (valid-hex? dec-values)
+      (compute-decimal dec-values)
       0)))
