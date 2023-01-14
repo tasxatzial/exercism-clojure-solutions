53a32ccf2684efcc79984bf401ed79c9f65d1bf5
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-14 04:37:27 +0200

    armstrong numbers: minor refactoring

--- armstrong-numbers/src/armstrong_numbers.clj
+++ armstrong-numbers/src/armstrong_numbers.clj
@@ -1,30 +1,34 @@
 (ns armstrong-numbers)
 
 (defn int->digits
-  "Converts an integer to a seq of its digits."
-  [s]
-  (map #(Character/digit ^char % 10) (str s)))
+  "Converts a non-negative integer to a seq of its digits."
+  [n]
+  (->> n
+       str
+       (map #(Character/digit ^char % 10))))
 
-;; Time complexity O(log2_N)
+;; Time complexity: O(log2_N)
 (defn math-pow
-  "Returns x^n."
+  "Returns x^n where n is integer > 0."
   [x n]
-  (let [multX (partial *' x)
-        sqr #(*' % %)]
+  (let [times-x (partial *' x)
+        square #(*' % %)]
     (loop [result []
            n n]
       (cond
         (= 1 n) (reduce #(%2 %1) x (rseq result))
-        (even? n) (recur (conj result sqr) (/ n 2))
-        :else (recur (conj result multX) (dec n))))))
+        (even? n) (recur (conj result square) (/ n 2))
+        :else (recur (conj result times-x) (dec n))))))
 
 (defn armstrong-sum
-  "For the given number n, it returns the sum of its digits each raised to the
-  power of the number of digits."
+  "For the given number n, it returns the sum of its digits each raised to
+  the power of the number of digits."
   [n]
   (let [digits (int->digits n)
         digit-count (count digits)]
-    (apply +' (map #(math-pow % digit-count) digits))))
+    (->> digits
+         (map #(math-pow % digit-count))
+         (apply +'))))
 
 (defn armstrong?
   [n]
