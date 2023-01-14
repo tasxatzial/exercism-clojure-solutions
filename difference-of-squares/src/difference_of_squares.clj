fa443dee40f89fc2b5f422029ad1716a935d490f
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-15 00:13:54 +0200

    difference of squares: update exercise & refactoring solution

--- difference-of-squares/src/difference_of_squares.clj
+++ difference-of-squares/src/difference_of_squares.clj
@@ -1,19 +1,25 @@
 (ns difference-of-squares)
 
+(defn square
+  [x]
+  (* x x))
+
 (defn sum-of-squares
-  "Returns the sum of squares from 1 to N."
   [N]
-  (apply + (map #(* % %) (range 1 (inc N)))))
+  (->> N
+       inc
+       (range 1)
+       (map square)
+       (apply +)))
 
 (defn square-of-sum
-  "Returns the square of the sum from 1 to N."
   [N]
-  (let [sum (apply + (range 1 (inc N)))]
-    (* sum sum)))
+  (->> N
+       inc
+       (range 1)
+       (apply +)
+       square))
 
 (defn difference
-  "Returns the difference between the square of the
-  sum and the sum of the squares of the first N
-  natural numbers."
   [N]
   (- (square-of-sum N) (sum-of-squares N)))
