31c5a72cf35db9f51711b1558545357bd9df4b83
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-14 21:57:34 +0200

    isogram: update exercise & remove old solution and add new one

--- isogram/src/isogram.clj
+++ isogram/src/isogram.clj
@@ -1,18 +1,10 @@
 (ns isogram)
 
-(defn collect-letters
-  "Collects the letters of lowercase s in a vector."
-  [s]
-  (let [lowercased (clojure.string/lower-case s)]
-    (reduce (fn [result c]
-              (if (or (= \space c) (= \- c))
-                result
-                (conj result c)))
-            [] lowercased)))
-
 (defn isogram?
-  "Returns true if s is isogram, false otherwise."
   [s]
-  (let [letters (collect-letters s)
-        letters-set (set letters)]
-    (= (count letters) (count letters-set))))
+  (->> s
+       clojure.string/lower-case
+       (filter #(Character/isAlphabetic (int %)))
+       frequencies
+       vals
+       (every? (partial = 1))))
