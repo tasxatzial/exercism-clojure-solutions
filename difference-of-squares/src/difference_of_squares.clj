b6d3d3d283874859037e3071c96e8f9446a0c0a1
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-15 17:20:37 +0200

    difference of squares: add closed-form formulas

--- difference-of-squares/src/difference_of_squares.clj
+++ difference-of-squares/src/difference_of_squares.clj
@@ -23,3 +23,18 @@
 (defn difference
   [N]
   (- (square-of-sum N) (sum-of-squares N)))
+
+;; solution 2: closed-form formulas
+(defn sum-of-squares2
+  [N]
+  (-> N
+      (* (inc N))
+      (* (inc (* 2 N)))
+      (/ 6)))
+
+(defn square-of-sum2
+  [N]
+  (-> N
+      (* (inc N))
+      (/ 2)
+      square))
