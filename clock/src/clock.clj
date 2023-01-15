1d560c68d39ce8791596b24563f564479a375d0b
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-16 00:21:34 +0200

    clock: update exercise & refactoring solution

--- clock/src/clock.clj
+++ clock/src/clock.clj
@@ -1,26 +1,29 @@
 (ns clock)
 
 (defn prefix-zero
-  "Prefixes n with 0 if n < 10 and returns the corresponding string."
+  "Prefixes the given integer with 0 if it is < 10.
+  Returns a string."
   [n]
   (if (< n 10)
     (str "0" n)
     (str n)))
 
 (defn clock->string
-  "Prefixes hour and/or minutes with a 0 (if they are < 10) and
-  returns the string hour:minutes"
-  [[hour minutes]]
-  (str (prefix-zero hour) ":" (prefix-zero minutes)))
+  "Converts a vector that represents a 24h clock to a string."
+  [v]
+  (let [[hours minutes] v]
+    (str (prefix-zero hours) ":" (prefix-zero minutes))))
 
 (defn clock
-  "Converts hours/minutes to 24h time."
+  "Converts the given hours and minutes to their equivalent 24h
+  values. Returns a vector."
   [hours minutes]
   (let [extra-hours (Math/floorDiv ^Integer minutes 60)]
     [(mod (+ extra-hours hours) 24)
      (mod minutes 60)]))
 
 (defn add-time
-  "Adds minutes to a 24h time."
-  [[hours minutes] extra-minutes]
-  (clock hours (+ minutes extra-minutes)))
+  "Adds the given minutes to a vector that represents a 24h clock."
+  [v extra-minutes]
+  (let [[hours minutes] v]
+    (clock hours (+ minutes extra-minutes))))
