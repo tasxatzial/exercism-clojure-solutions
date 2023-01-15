4d9ce3e36f19bb7ccf7c6e70c67e90a292d419b7
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-16 01:03:22 +0200

    matching brackets: update exercise & refactoring solution

--- matching-brackets/src/matching_brackets.clj
+++ matching-brackets/src/matching_brackets.clj
@@ -1,28 +1,30 @@
 (ns matching-brackets)
 
-;; using a vector as stack
+(def get-closing-bracket {\{ \},
+                          \( \),
+                          \[ \]})
 
-(def brackets #{\{ \} \( \) \[ \]})
-(def close-bracket {\{ \}
-                    \( \)
-                    \[ \]})
+(def brackets (-> #{}
+                  (into (keys get-closing-bracket))
+                  (into (vals get-closing-bracket))))
 
-(defn open-bracket?
-  "Returns true if c is an opening bracket."
+(defn opening-bracket?
+  "Returns true if c is an opening bracket, else false."
   [c]
-  (contains? close-bracket c))
+  (contains? get-closing-bracket c))
 
+;; using a vector as stack
 (defn valid?
   "Returns true if the given string has properly
-  matched brackets, false otherwise."
+  matched brackets, else false."
   [s]
   (loop [stack []
-         [bracket & rest-brackets] (filter brackets s)]
-    (if bracket
-      (if (open-bracket? bracket)
-        (recur (conj stack bracket) rest-brackets)
-        (let [prev-bracket (peek stack)]
-          (if (= bracket (close-bracket prev-bracket))
+         [curr-bracket & rest-brackets] (filter brackets s)]
+    (if curr-bracket
+      (if (opening-bracket? curr-bracket)
+        (recur (conj stack curr-bracket) rest-brackets)
+        (let [stack-top-bracket (peek stack)]
+          (if (= curr-bracket (get-closing-bracket stack-top-bracket))
             (recur (pop stack) rest-brackets)
             false)))
       (empty? stack))))
