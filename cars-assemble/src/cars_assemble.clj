4b16beda97c3fc91516c9f9f4c21521ae38a6567
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-15 17:47:32 +0200

    cars assemble

---
+++ cars-assemble/src/cars_assemble.clj
@@ -0,0 +1,30 @@
+(ns cars-assemble)
+
+(def get-success-rate
+  {0 0,
+   1 1, 2 1, 3 1, 4 1,
+   5 0.9, 6 0.9, 7 0.9, 8 0.9,
+   9 0.8,
+   10 0.77})
+
+(defn get-produced-cars
+  "Returns the total produced cards per hour."
+  [speed]
+  (* 221.0 speed))
+
+(defn production-rate
+  "Returns the assembly line's production rate per hour,
+   taking into account its success rate."
+  [speed]
+  (let [success-rate (get-success-rate speed)
+        produced-cars (get-produced-cars speed)]
+    (* success-rate produced-cars)))
+
+(defn working-items
+  "Calculates how many working cars are produced per minute."
+  [speed]
+  (-> speed
+      production-rate
+      (/ 60)
+      Math/floor
+      Math/round))
