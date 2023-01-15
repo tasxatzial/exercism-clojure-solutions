187721bc715748c2947e88cb95385233163abc99
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-16 01:40:05 +0200

    queen attack: update exercise & refactoring solution

--- queen-attack/src/queen_attack.clj
+++ queen-attack/src/queen_attack.clj
@@ -3,29 +3,31 @@
 (def empty-board
   (apply str (take 8 (repeat "_ _ _ _ _ _ _ _\n"))))
 
-(defn vec-pos
+(defn get-board-index
   "Transforms a (x,y) board position to the corresponding
-  vector position."
+  one dimensional index. Both x and y take values from
+  0 to 7."
   [x y]
   (+ (* 16 x) (* 2 y)))
 
 (defn board-string
-  "Places white and black queens on the board in the
-  positions specified by the given map."
+  "Places white and black queens on the board using the
+  given positions map."
   [positions]
-  (if (seq positions)
+  (if (empty? positions)
+    empty-board
     (let [[wx wy] (:w positions)
-          [bx by] (:b positions)
-          board (vec empty-board)
-          w-board (assoc board (vec-pos wx wy) \W)
-          wb-board (assoc w-board (vec-pos bx by) \B)]
-      (apply str wb-board))
-    empty-board))
+          [bx by] (:b positions)]
+      (apply str (-> (vec empty-board)
+                     (assoc (get-board-index wx wy) \W)
+                     (assoc (get-board-index bx by) \B))))))
 
 (defn can-attack
-  "Returns true if the queens in the positions specified
-  by the given map can attack each other, false otherwise."
+  "Returns true if the queens in the given positions map can
+  attack each other, else false."
   [positions]
   (let [[wx wy] (:w positions)
         [bx by] (:b positions)]
-    (or (= wx bx) (= wy by) (= (- wx wy) (- bx by)))))
+    (or (= wx bx)
+        (= wy by)
+        (= (Math/abs ^int (- wx bx)) (Math/abs ^int (- wy by))))))
