56b79c53a7236b1678e2361f12224bdd2c724a28
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-17 01:33:25 +0200

    isbn verifier: update exercise & remove old solution and add new one

--- isbn-verifier/src/isbn_verifier.clj
+++ isbn-verifier/src/isbn_verifier.clj
@@ -1,47 +1,41 @@
 (ns isbn-verifier)
 
-(defn digit?
-  "Returns true if character c represents a digit,
-  false otherwise."
+(defn get-char-val
+  "Returns the numeric value of a char."
   [c]
-  (<= 48 (int c) 57))
-
-(defn valid-left-part?
-  "Checks if the left part of the isbn is valid. It must have
-  9 digit characters."
-  [isbn-left]
-  (and (= 9 (count isbn-left))
-       (every? digit? isbn-left)))
-
-(defn left-part-value
-  "Computes the numerical value of the left part of the
-  isbn (9 chars)."
-  [left-part]
-  (let [digits (map #(Character/digit ^char % 10) left-part)]
-    (apply + (map * digits (range 10 1 -1)))))
-
-(defn valid-right-char?
-  "Checks if the rightmost char of the isbn is valid."
-  [c]
-  (or (= \X c) (digit? c)))
-
-(defn right-char-value
-  "Returns the numerical value of the  rightmost char of
-  the isbn."
-  [c]
-  (if (= \X c)
+  (if (= c \X)
     10
-    (Character/digit ^char c 10)))
+    (- (int c) 48)))
+
+(defn valid-isbn-pattern?
+  "Returns true if the given string is a valid ISBN pattern,
+  else false."
+  [s]
+  (->> s
+       (re-matches #"\d{9}(X|\d)|\d-\d{3}-\d{5}-(X|\d)")
+       boolean))
+
+(defn isbn-val
+  "Computes the value of a string according to the ISBN
+  formula. The string must be a valid ISBN pattern."
+  [s]
+  (->> s
+       (remove #(= % \-))
+       (map get-char-val)
+       (map * (range 10 0 -1))
+       (apply +)))
+
+(defn valid-isbn-val?
+  "Returns true if the value of the string according to
+  the ISBN formula is valid, else false. The string must be
+  a valid ISBN pattern."
+  [s]
+  (-> s
+      isbn-val
+      (mod 11)
+      zero?))
 
 (defn isbn?
-  "Checks whether an isbn is valid."
-  [isbn]
-  (let [no-dashes (remove #{\-} isbn)
-        left-part (butlast no-dashes)
-        right-char (last no-dashes)]
-    (if (and (valid-left-part? left-part)
-             (valid-right-char? right-char))
-      (let [lval (left-part-value left-part)
-            rval (right-char-value right-char)]
-        (= 0 (mod (+ lval rval) 11)))
-      false)))
+  "Returns true if the given string represents a valid ISBN."
+  [s]
+  (and (valid-isbn-pattern? s) (valid-isbn-val? s)))
