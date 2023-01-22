b069f65e7de8d3b2404c80fd459bc2c4d089fe00
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-22 05:25:45 +0200

    binary search: add second solution

--- binary-search/src/binary_search.clj
+++ binary-search/src/binary_search.clj
@@ -1,5 +1,11 @@
 (ns binary-search)
 
+(defn middle
+  [coll]
+  (quot (count coll) 2))
+
+;; solution 1:
+;; Convert to vector and recusively update low & high indices.
 (defn search-for
   [n coll]
   (let [coll (vec coll)]
@@ -14,6 +20,20 @@
             (> mid-item n) (recur low-idx (dec mid-index))
             :else (recur (inc mid-index) high-idx)))))))
 
-(defn middle
-  [coll]
-  (quot (count coll) 2))
+;; solution 2:
+;; Recursively search in a smaller sublist. Requires only one index.
+(defn search-for2
+  [key coll]
+  (loop [coll-idx 0
+         subcoll coll]
+    (if (empty? subcoll)
+      (throw (Exception. "not found"))
+      (let [subcoll-mid-idx (middle subcoll)
+            subcoll-mid-el (nth subcoll subcoll-mid-idx)]
+        (cond
+          (= key subcoll-mid-el)
+            (+ coll-idx subcoll-mid-idx)
+          (< key subcoll-mid-el)
+            (recur coll-idx (take subcoll-mid-idx subcoll))
+          :else (recur (+ coll-idx (inc subcoll-mid-idx))
+                       (drop (inc subcoll-mid-idx) subcoll)))))))
