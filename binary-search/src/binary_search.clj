dbafac2a3ab91d2b63ba3d51097b6acf8db506eb
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-22 04:59:58 +0200

    binary search: refactoring

--- binary-search/src/binary_search.clj
+++ binary-search/src/binary_search.clj
@@ -1,18 +1,19 @@
 (ns binary-search)
 
 (defn search-for
-  ([n coll]
-   (search-for n (vec coll) 0 (dec (count coll))))
-  ([n coll low-index high-index]
-   (if (> low-index high-index)
-     (throw (Exception. "not found"))
-     (let [mid-index (Math/round (Math/floor (/ (+ high-index low-index) 2)))
-           mid-item (get coll mid-index)]
-       (cond
-         (= n mid-item) mid-index
-         (> mid-item n) (recur n coll low-index (dec mid-index))
-         :else (recur n coll (inc mid-index) high-index))))))
+  [n coll]
+  (let [coll (vec coll)]
+    (loop [low-idx 0
+           high-idx (dec (count coll))]
+      (if (> low-idx high-idx)
+        (throw (Exception. "not found"))
+        (let [mid-index (quot (+ high-idx low-idx) 2)
+              mid-item (get coll mid-index)]
+          (cond
+            (= n mid-item) mid-index
+            (> mid-item n) (recur low-idx (dec mid-index))
+            :else (recur (inc mid-index) high-idx)))))))
 
 (defn middle
   [coll]
-  (Math/round (Math/floor (/ (count coll) 2))))
+  (quot (count coll) 2))
