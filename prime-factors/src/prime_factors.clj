aa5dee7dd4771a24d3e9ca98c15cf8bd70c30cd6
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-29 02:53:04 +0200

    prime factors: refactoring

--- prime-factors/src/prime_factors.clj
+++ prime-factors/src/prime_factors.clj
@@ -1,52 +1,47 @@
 (ns prime-factors)
 
-;; optimized prime-factors
-
-(defn- composites
-  "Returns a lazy seq of composites in [2, max].
-  Even numbers are left out."
-  [max]
-  (for [i (range 3 (inc (Math/round (Math/sqrt max))) 2)
-        k (range i (inc (/ max i)) 2)]
+(defn get-non-even-composites
+  "Returns a lazy seq of composites in [2, n]. Even numbers are excluded."
+  [n]
+  (for [i (range 3 (inc (Math/round (Math/sqrt n))) 2)
+        k (range i (inc (/ n i)) 2)]
     (* i k)))
 
 (defn sieve
-  "Returns all primes in [2, max]. "
-  [max]
-  (if (< max 2)
+  "Returns all primes in [2, n]. "
+  [n]
+  (if (< n 2)
     ()
-    (vec (remove (set (composites max))
-                 (cons 2 (range 3 (inc max) 2))))))
+    (let [candidates (cons 2 (range 3 (inc n) 2))
+          composites (get-non-even-composites n)]
+      (vec (remove (set composites) candidates)))))
 
-(defn- prime?
-  "Returns false if n has a factor in the given list
-  of primes or if n = 1. Returns true otherwise."
+(defn composite?
+  "Returns true if n has any of the primes as a factor, else false.
+  Primes includes all primes which are <= square root of n."
   [n primes]
-  (if (= n 1)
-    false
-    (every? #(not= 0 (mod n %))
-            primes)))
+  (->> primes
+       (take-while #(< % (inc (Math/sqrt n))))
+       (some #(zero? (mod n %)))
+       boolean))
 
 (defn of
-  "Returns the prime factors of n. Some of them may be
-  repeated more than 1 time."
-  ([n]
-   (if (= n 1)
-     []
-     (let [primes (sieve (Math/round (Math/sqrt n)))]
-       (of n primes))))
-  ([n primes]
-   (loop [result []
-          num n
-          i 0]
-     (let [nth-prime (get primes i)]
-       (if nth-prime
-         (let [q (/ num nth-prime)]
-           (if (int? q)
-             (if (prime? q primes)
-               (recur (conj result nth-prime q) q i)
-               (recur (conj result nth-prime) q i))
-             (recur result num (inc i))))
-         (if (seq result)
-           result
-           [n]))))))
+  "Returns the prime factors of n. Each factor appears as many times
+  as the power of the factor in the prime decomposition of n."
+  [n]
+  (if (= n 1)
+    []
+    (let [primes (sieve (Math/round (Math/sqrt n)))]
+      (loop [result []
+             num n
+             i 0]
+        (if-let [nth-prime (get primes i)]
+          (let [quotient (/ num nth-prime)]
+            (if (int? quotient)
+              (if (composite? quotient primes)
+                (recur (conj result nth-prime) quotient i)
+                (if (= 1 quotient)
+                  (conj result nth-prime)
+                  (conj result nth-prime quotient)))
+              (recur result num (inc i))))
+          (or (seq result) [n]))))))
