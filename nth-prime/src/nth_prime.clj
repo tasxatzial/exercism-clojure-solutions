1347cc6b813919761f4e8386b8a9f56e6d58f849
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-02-06 08:26:37 +0200

    nth-prime: remove second solution, refactoring first solution

--- nth-prime/src/nth_prime.clj
+++ nth-prime/src/nth_prime.clj
@@ -1,15 +1,17 @@
 (ns nth-prime)
 
-;; solution 1
-
-(defn composite?
-  "Returns true if n has any of the primes as a factor, else false.
-  Primes includes all primes which are <= square root of n."
-  [n primes]
-  (->> primes
-       (take-while #(< % (inc (Math/sqrt n))))
-       (some #(zero? (mod n %)))
-       boolean))
+(defn prime?
+  "Returns true if N is prime. This will happen if N is a multiple
+  of any number in the given list of primes."
+  [N primes]
+  (reduce (fn [result prime]
+            (if (> prime (inc (Math/sqrt N)))
+              (reduced true)
+              (if (= 0 (mod N prime))
+                (reduced false)
+                result)))
+          true
+          primes))
 
 (defn primes
   "Returns all primes in [2, n]."
@@ -19,48 +21,12 @@
          i 2]
     (if (zero? n)
       result
-      (if (composite? i result)
-        (recur n result (inc i))
-        (recur (dec n) (conj result i) (inc i))))))
+      (if (prime? i result)
+        (recur (dec n) (conj result i) (inc i))
+        (recur n result (inc i))))))
 
 (defn nth-prime
   [n]
   (if (zero? n)
     (throw (IllegalArgumentException.))
     (last (primes n))))
-
-;; ---------------------------------------------
-;; solution 2: sieve
-;; this is a lot faster for large n
-
-(defn get-non-even-composites
-  "Returns a lazy seq of composites in [2, n]. Even numbers are excluded."
-  [n]
-  (for [i (range 3 (inc (Math/round (Math/floor (Math/sqrt n)))) 2)
-        k (range i (inc (Math/round (Math/floor (/ n i)))) 2)]
-    (* i k)))
-
-(defn sieve
-  "Returns all primes in [2, n]. "
-  [n]
-  (if (< n 2)
-    ()
-    (let [candidates (cons 2 (range 3 (inc n) 2))
-          composites (get-non-even-composites n)]
-      (remove (set composites) candidates))))
-
-(defn sieve-range
-  "Returns a range that contains the nth prime. Valid for n>= 6."
-  [n]
-  (let [b (+ (* (Math/log n) n)
-             (* (Math/log (Math/log n)) n))]
-    [(- b n) b]))
-
-(defn nth-prime
-  [n]
-  (cond
-    (zero? n) (throw (IllegalArgumentException.))
-    (< n 6) (get [2 3 5 7 11] (dec n))
-    :else (let [upper-lim (second (sieve-range n))
-                primes (sieve upper-lim)]
-            (nth primes (dec n)))))
