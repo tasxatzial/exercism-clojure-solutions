c51a8a69e132840afe04391dcf12bac220a452f4
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-02-07 23:07:56 +0200

    octal: refactoring

--- octal/src/octal.clj
+++ octal/src/octal.clj
@@ -21,17 +21,13 @@
   "Given a collection of decimal values (0 <= val < 8) that represents
   an octal number, it computes the number in base 10."
   [decimal-values]
-  (+ (last decimal-values)
-     (reduce (fn [result digit]
-               (* 8 (+ result digit)))
-             0
-             (butlast decimal-values))))
+  (when (valid-oct? decimal-values)
+    (reduce #(+ %2 (* 8 %1))
+            0 decimal-values)))
 
 (defn to-decimal
   "Converts the given octal number (string) to the decimal equivalent.
   Returns 0 if the string does not represent a valid octal number."
   [s]
   (let [dec-values (oct->dec-values s)]
-    (if (valid-oct? dec-values)
-      (compute-decimal dec-values)
-      0)))
+    (or (compute-decimal dec-values) 0)))
