62ae000b8511121b8f2826dc7578096fd897bfc0
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-02-07 18:29:39 +0200

    all your base: refactoring

--- all-your-base/src/all_your_base.clj
+++ all-your-base/src/all_your_base.clj
@@ -1,36 +1,45 @@
 (ns all-your-base)
 
+(defn valid-base?
+  "Returns true if the given base is an integer > 1, else false."
+  [b]
+  (and (> b 1) (integer? b)))
+
 (defn valid-num?
-  "Returns true if the collection of decimal values represents a valid
-  number in the given base, false otherwise."
-  [decimal-vals base]
-  (every? #(and (>= % 0) (< % base))
-          decimal-vals))
+  "Multi-arity function;
+  1) If called with 1 args: Returns true if the given number is integer >= 0,
+  else false.
+  2) If called with 2 args: Returns true if the collection of decimal values
+  represents a valid number in the given base, else false."
+  ([n]
+   (and (integer? n) (>= n 0)))
+  ([decimal-vals base]
+   (and (seq decimal-vals)
+        (every? #(and (integer? %) (>= % 0) (< % base))
+                decimal-vals))))
 
 (defn to-base10
-  "Given a collection of decimal values (0 <= val < base) that represents
+  "Given a collection of decimal values that represent
   a number in the given base, it computes the number in base 10."
   [decimal-vals base]
-  (when (and (seq decimal-vals) (> base 1) (valid-num? decimal-vals base))
-    (+ (reduce #(* base (+ %1 %2))
-               0
-               (butlast decimal-vals))
-       (last decimal-vals))))
+  (when (and (valid-num? decimal-vals base) (valid-base? base))
+    (reduce #(+ %2 (* base %1))
+            0 decimal-vals)))
 
 (defn from-base10
-  "Converts a non-negative base 10 number to a seq of its decimal
-  values in the given base."
+  "Converts a base 10 number to a list of its decimal values in the given base."
   [n base]
-  (when (and (not (nil? n)) (> base 1))
-    (loop [result '()
+  (when (and (valid-num? n) (valid-base? base))
+    (loop [result ()
            n n]
       (if (zero? n)
         (or (seq result) '(0))
-        (let [remainder (rem n base)
-              quotient (quot n base)]
-          (recur (cons remainder result) quotient))))))
+        (let [r (rem n base)
+              q (quot n base)]
+          (recur (conj result r) q))))))
 
 (defn convert
   [src-base digits target-base]
-  (-> (to-base10 digits src-base)
+  (-> digits
+      (to-base10 src-base)
       (from-base10 target-base)))
