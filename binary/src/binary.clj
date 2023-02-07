d84fcd76b6021088670de0b6b31cf071067adfc5
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-02-07 23:00:42 +0200

    binary: refactoring

--- binary/src/binary.clj
+++ binary/src/binary.clj
@@ -21,17 +21,13 @@
   "Given a collection of decimal values (0 <= val < 2) that represents
   a binary number, it computes the number in base 10."
   [decimal-values]
-  (+ (last decimal-values)
-     (reduce (fn [result digit]
-               (* 2 (+ result digit)))
-             0
-             (butlast decimal-values))))
+  (when (valid-bin? decimal-values)
+    (reduce #(+ %2 (* 2 %1))
+            0 decimal-values)))
 
 (defn to-decimal
   "Converts the given binary number (string) to the decimal equivalent.
   Returns 0 if the string does not represent a valid binary number."
   [s]
   (let [dec-values (bin->dec-values s)]
-    (if (valid-bin? dec-values)
-      (compute-decimal dec-values)
-      0)))
+    (or (compute-decimal dec-values) 0)))
