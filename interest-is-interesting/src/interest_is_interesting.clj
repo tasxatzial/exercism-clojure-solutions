13369fa69dd87cee0de43bb8b33f1d0c737fc368
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-03-18 19:10:31 +0200

    interest-is-interesting: refactoring

--- interest-is-interesting/src/interest_is_interesting.clj
+++ interest-is-interesting/src/interest_is_interesting.clj
@@ -6,8 +6,13 @@
     (- x)
     x))
 
+(defn percentage->double
+  [percentage]
+  (* 0.01 percentage))
+
 (defn interest-rate
-  "Calculates the interest rate based on the specified balance."
+  "Calculates the interest rate based on the specified balance.
+  The rate represents a percentage, e.g. 0.5 is 0.5 percent."
   [balance]
   (cond
     (neg? balance) -3.213
@@ -15,20 +20,27 @@
     (< balance 5000) 1.621
     :else 2.475))
 
+(defn extra-money
+  "Calculates the amount of money that will be added to the current
+  balance, taking into account the interest rate. The amount can be
+  negative or positive."
+  [balance]
+  (-> balance
+      interest-rate
+      percentage->double
+      bigdec
+      (* (abs balance))))
+
 (defn annual-balance-update
   "Calculates the annual balance update, taking into account the
   interest rate."
   [balance]
-  (-> balance
-      interest-rate
-      bigdec
-      (* (abs balance) 0.01M)                               ;(.abs balance) works too
-      (+ balance)))
+  (+ balance (extra-money balance)))
 
 (defn amount-to-donate
   "Calculates how much money to donate to charities based on the
   balance and the tax-free percentage."
   [balance tax-free-percentage]
-  (if (> balance 0)
-    (int (* 0.02 balance tax-free-percentage))
-    0))
+  (int (if (> balance 0)
+         (* 2 balance (percentage->double tax-free-percentage))
+         0)))
