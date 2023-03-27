b947908ef62dc462e33ce5f07b95474afe936cb9
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-03-27 21:49:03 +0300

    nucleotide-count: refactoring

--- nucleotide-count/src/nucleotide_count.clj
+++ nucleotide-count/src/nucleotide_count.clj
@@ -4,16 +4,17 @@
 
 (defn count-of-nucleotide-in-strand
   [nucleotide strand]
-  (if (not (contains? nucleotides nucleotide))
-    (throw (Exception.))
-    (count (filter #{nucleotide} strand))))
+  (if (contains? nucleotides nucleotide)
+    (count (filter #{nucleotide} strand))
+    (throw (Exception.))))
 
 (defn nucleotide-counts
   "Returns a map of {nucleotide -> nucleotide count} for all
   nucleotides in the given strand."
   [strand]
   (reduce (fn [result nucleotide]
-            (let [nucleotide-count (count-of-nucleotide-in-strand nucleotide strand)]
-              (assoc result nucleotide nucleotide-count)))
+            (->> strand
+                 (count-of-nucleotide-in-strand nucleotide)
+                 (assoc result nucleotide)))
           {}
           nucleotides))
