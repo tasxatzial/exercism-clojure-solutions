a3ca1192efefcbc220da733d8c1d77b715622116
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-04-02 22:56:49 +0300

    reverse string: remove trivial solution

--- reverse-string/src/reverse_string.clj
+++ reverse-string/src/reverse_string.clj
@@ -1,11 +1,5 @@
 (ns reverse-string)
 
-;; solution 1
 (defn reverse-string
   [s]
   (apply str (into () s)))
-
-;; solution 2: built-in!
-(defn reverse-string2
-  [s]
-  (clojure.string/reverse s))
