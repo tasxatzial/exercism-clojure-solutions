3b0f4ec14a37f49e1afb124e1308b3bf9bf0ce97
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-05-22 11:21:09 +0300

    sublist: refactoring

--- sublist/src/sublist.clj
+++ sublist/src/sublist.clj
@@ -1,16 +1,16 @@
 (ns sublist)
 
-(defn sublist?
-  "Returns true if l2 is a sublist of l1, else false."
+(defn- sublist?
+  "Returns true if l1 is a sublist of l2, else false. Assumes l2 is not empty."
   [l1 l2]
-  (->> (partition (count l2) 1 l1)
-       (some #(= l2 %))
+  (->> (partition (count l1) 1 l2)
+       (some #(= l1 %))
        true?))
 
 (defn classify
   [l1 l2]
   (cond
     (= l1 l2) :equal
-    (sublist? l2 l1) :sublist
-    (sublist? l1 l2) :superlist
+    (sublist? l2 l1) :superlist
+    (sublist? l1 l2) :sublist
     :else :unequal))
