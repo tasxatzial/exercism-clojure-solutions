fc074e4eb9ca8ca1cb353de3d4361899f4fb74ed
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-06-13 17:39:25 +0300

    run-length-encoding: refactoring & use transducer in encoding

--- run-length-encoding/src/run_length_encoding.clj
+++ run-length-encoding/src/run_length_encoding.clj
@@ -1,36 +1,40 @@
 (ns run-length-encoding)
 
-(defn encode-single
-  "Takes a string that consists of the same char and
-  returns the compressed equivalent."
+(defn encode-rule
+  "Encodes a sequence consisting of the same characters.
+  E.g. (A A A) -> 3A"
   [coll]
-  (let [coll-count (count coll)]
-    (if (> coll-count 1)
-      (str coll-count (first coll))
+  (let [cnt (count coll)]
+    (if (> cnt 1)
+      (str cnt (first coll))
       (str (first coll)))))
 
-(defn decode-single
-  "Takes a string that represents the compressed form of a
-  sequence that consists of the same char and returns the
-  decompressed equivalent."
+(defn decode-rule
+  "Decodes an encoded string consisting of only one character.
+  E.g. 3A -> AAA"
   [s]
-  (let [string-char (re-find #"\D" s)]
-    (if-let [string-num (re-find #"\d+" s)]
-      (->> string-char
-           (repeat (Integer/parseInt string-num))
+  (let [ch (re-find #"\D" s)]
+    (if-let [num (re-find #"\d+" s)]
+      (->> ch
+           (repeat (Integer/parseInt num))
            (apply str))
-      string-char)))
+      ch)))
+
+(def encode-transform
+  "Encoding transducer. Transforms a string to its encoded chunks."
+  (comp
+    (partition-by identity)
+    (map encode-rule)))
 
 (defn run-length-encode
   [s]
   (->> s
-       (partition-by identity)
-       (map encode-single)
+       (transduce encode-transform conj)
        (apply str)))
 
 (defn run-length-decode
   [s]
   (->> s
        (re-seq #"\d*.")
-       (map decode-single)
+       (map decode-rule)
        (apply str)))
