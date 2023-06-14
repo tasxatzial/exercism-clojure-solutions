7838b91d05c9bb760bce869367c8160a6c1f4000
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-06-14 04:28:50 +0300

    rotational-cipher: refactoring

--- rotational-cipher/src/rotational_cipher.clj
+++ rotational-cipher/src/rotational_cipher.clj
@@ -1,39 +1,22 @@
 (ns rotational-cipher)
 
-(defn get-char-type
-  "Returns:
-  1) :uppercase if ascii val corresponds to an uppercase letter
-  2) :lowercase if ascii val corresponds to a lowercase letter
-  3) :other in all other cases."
-  [ascii-val]
-  (cond
-    (<= 97 ascii-val 122) :uppercase
-    (<= 65 ascii-val 90) :lowercase
-    :else :other))
-
 (defn rotate-int
-  "Rotates num by the given amount. Num should satisfy
-  lowest-int <= num <= lowest-int + cycle-length."
-  [lowest-int cycle-length num amount]
-  (+ lowest-int (mod (+ amount (- num lowest-int)) cycle-length)))
-
-;; rotate functions based on char type
-(def rotate-fn
-  {:lowercase (partial rotate-int 65 26)
-   :uppercase (partial rotate-int 97 26)})
+  "Rotates num by n. Num should be in [low, low + 26]"
+  [low num n]
+  (+ low (mod (+ n (- num low)) 26)))
 
 (defn rotate-char
   "Rotates the char c by n."
   [n c]
-  (let [ascii-val (int c)
-        char-type (get-char-type ascii-val)]
-    (if-let [rotate-fn (rotate-fn char-type)]
-      (char (rotate-fn ascii-val n))
+  (let [ascii-val (int c)]
+    (if (Character/isLetter ascii-val)
+      (if (Character/isLowerCase ascii-val)
+        (char (rotate-int 97 ascii-val n))
+        (char (rotate-int 65 ascii-val n)))
       c)))
 
 (defn rotate
   [s n]
-  (let [rotate-char-by-n (partial rotate-char n)]
-    (->> s
-         (map rotate-char-by-n)
-         (apply str))))
+  (->> s
+       (map #(rotate-char n %))
+       (apply str)))
