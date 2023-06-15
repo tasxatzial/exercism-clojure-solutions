c89d3a417eb1c3188a9fee45839c974aa8d385f6
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-06-15 17:13:25 +0300

    atbash-cipher: refactoring to use transducer

--- atbash-cipher/src/atbash_cipher.clj
+++ atbash-cipher/src/atbash_cipher.clj
@@ -3,13 +3,17 @@
 (def alphabet "abcdefghijklmnopqrstuvwxyz")
 (def alphabet-encode-map (zipmap alphabet (reverse alphabet)))
 
+(def encode-transform
+  "Encoding transducer for lowercased strings. Encodes input into strings of fixed length (5)."
+  (comp
+    (filter #(or (Character/isDigit ^char %) (Character/isLetter ^char %)))
+    (map #(get alphabet-encode-map % %))
+    (partition-all 5)
+    (map #(apply str %))))
+
 (defn encode
   [s]
   (->> s
        clojure.string/lower-case
-       (re-seq #"[A-Za-z0-9]")
-       (apply str)
-       (map #(get alphabet-encode-map % %))
-       (partition-all 5)
-       (map #(apply str %))
+       (transduce encode-transform conj)
        (clojure.string/join " ")))
