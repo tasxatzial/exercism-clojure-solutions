3b2fe8895e3b8b60753291ae77740ed6066718e5
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-06-17 02:51:01 +0300

    anagram: name refactoring

--- anagram/src/anagram.clj
+++ anagram/src/anagram.clj
@@ -1,16 +1,16 @@
 (ns anagram
   (:require [clojure.string :as str]))
 
-(defn- _anagram?
+(defn- anagram?
   "Returns true if candidate is an anagram of word. Assumes word is in lowercase."
   [word word-freqs candidate]
-  (let [lowercase-candidate (str/lower-case candidate)]
-    (and (not= word lowercase-candidate)
-         (= word-freqs (frequencies lowercase-candidate)))))
+  (let [lcase-candidate (str/lower-case candidate)]
+    (and (not= word lcase-candidate)
+         (= word-freqs (frequencies lcase-candidate)))))
 
 (defn anagrams-for
   [word candidates]
-  (let [lowercase-word (str/lower-case word)
-        lowercase-word-freqs (frequencies lowercase-word)
-        word-anagram? (partial _anagram? lowercase-word lowercase-word-freqs)]
+  (let [lcase-word (str/lower-case word)
+        word-freqs (frequencies lcase-word)
+        word-anagram? (partial anagram? lcase-word word-freqs)]
     (filter word-anagram? candidates)))
