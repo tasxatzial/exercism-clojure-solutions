2aa66ac6e714390898fc4041f22c4343208df1cf
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-06-26 01:58:56 +0300

    triangle: refactoring to use ==

--- triangle/src/triangle.clj
+++ triangle/src/triangle.clj
@@ -1,31 +1,29 @@
 (ns triangle)
 
-(defn valid-triangle?
-  "Returns true if a triangle with sides n1 n2 n3 is valid,
-  false otherwise."
-  [n1 n2 n3]
-  (and (pos? n1)
-       (pos? n2)
-       (pos? n3)
-       (> (+ n1 n2) n3)
-       (> (+ n1 n3) n2)
-       (> (+ n2 n3) n1)))
+(defn is-valid?
+  [a b c]
+  (and (pos? a)
+       (pos? b)
+       (pos? c)
+       (> (+ a b) c)
+       (> (+ a c) b)
+       (> (+ b c) a)))
 
 (defn equilateral?
-  [n1 n2 n3]
-  (and (valid-triangle? n1 n2 n3)
-       (= n1 n2 n3)))
+  [a b c]
+  (and (is-valid? a b c)
+       (== a b c)))
 
 (defn isosceles?
-  [n1 n2 n3]
-  (and (valid-triangle? n1 n2 n3)
-       (or (= n1 n2)
-           (= n1 n3)
-           (= n2 n3))))
+  [a b c]
+  (and (is-valid? a b c)
+       (or (== a b)
+           (== a c)
+           (== b c))))
 
 (defn scalene?
-  [n1 n2 n3]
-  (and (valid-triangle? n1 n2 n3)
-       (not= n1 n2)
-       (not= n1 n3)
-       (not= n2 n3)))
+  [a b c]
+  (and (is-valid? a b c)
+       (not (or (== a b)
+                (== a c)
+                (== b c)))))
