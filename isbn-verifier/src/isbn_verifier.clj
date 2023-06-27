2f3b094b64b388ac94dda0ec9bd6432733147e17
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-06-27 03:18:52 +0300

    isbn-verifier: refactoring

--- isbn-verifier/src/isbn_verifier.clj
+++ isbn-verifier/src/isbn_verifier.clj
@@ -2,40 +2,27 @@
 
 (defn get-char-val
   "Returns the numeric value of a char."
-  [c]
-  (if (= c \X)
+  [ch]
+  (if (= ch \X)
     10
-    (- (int c) 48)))
+    (Character/digit ^char ch 10)))
 
 (defn valid-isbn-pattern?
-  "Returns true if the given string is a valid ISBN pattern,
-  else false."
+  "Returns true if the given string is a valid ISBN pattern, else false."
   [s]
-  (->> s
-       (re-matches #"\d{9}(X|\d)|\d-\d{3}-\d{5}-(X|\d)")
-       boolean))
+  (boolean (re-matches #"\d{9}(X|\d)|\d-\d{3}-\d{5}-(X|\d)" s)))
 
 (defn isbn-val
-  "Computes the value of a string according to the ISBN
-  formula. The string must be a valid ISBN pattern."
+  "Computes the value of a string according to the ISBN formula.
+  The string must be a valid ISBN pattern."
   [s]
-  (->> s
-       (remove #(= % \-))
+  (->> (remove #(= % \-) s)
        (map get-char-val)
        (map * (range 10 0 -1))
        (reduce +)))
 
-(defn valid-isbn-val?
-  "Returns true if the value of the string according to
-  the ISBN formula is valid, else false. The string must be
-  a valid ISBN pattern."
-  [s]
-  (-> s
-      isbn-val
-      (mod 11)
-      zero?))
-
 (defn isbn?
   "Returns true if the given string represents a valid ISBN."
   [s]
-  (and (valid-isbn-pattern? s) (valid-isbn-val? s)))
+  (and (valid-isbn-pattern? s)
+       (zero? (mod (isbn-val s) 11))))
