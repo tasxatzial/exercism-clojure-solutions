67c0c6a8bccca50df98f6e30dbd0578af3ab66e4
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-07-15 21:07:15 +0300

    sublist: refactor both solutions

--- sublist/src/sublist.clj
+++ sublist/src/sublist.clj
@@ -1,16 +1,21 @@
 (ns sublist)
 
-(defn- sublist?
-  "Returns true if l1 is a sublist of l2, else false. Assumes l2 is not empty."
+;; Solution 1
+;; Naive approach. Inefficient!
+
+(defn sublist?
+  "Returns true if l1 is a sublist of l2, else false."
   [l1 l2]
-  (->> (partition (count l1) 1 l2)
-       (some #(= l1 %))
-       true?))
+  (if (seq l2)
+    (->> (partition (count l1) 1 l2)
+         (some #{l1})
+         some?)
+    (not (seq l1))))
 
 (defn classify
   [l1 l2]
   (cond
     (= l1 l2) :equal
-    (sublist? l2 l1) :superlist
     (sublist? l1 l2) :sublist
+    (sublist? l2 l1) :superlist
     :else :unequal))
--- sublist_2/src/sublist.clj
+++ sublist_2/src/sublist.clj
@@ -1,13 +1,15 @@
 (ns sublist)
 
+;; Solution 2
 ;; This approach employs a similar concept to solutions that utilize 'partition'.
 ;; However, there are two differences:
 ;; 1) The classify function calls sublist? only once.
 ;; 2) It never constructs any subsequences.
-;; As a result, it exhibits a speed improvement of 3-4 orders of magnitude.
+;; As a result, it is way more efficient.
 
-(defn- p-equals?
-  "Returns true if all elements of l1 match the corresponding elements of l2."
+(defn sub-equals?
+  "Returns true if l1 equals the list formed by the first (count l1)
+  elements of l2."
   [l1 l2]
   (if-let [l1-seq (seq l1)]
     (if (= (first l1-seq) (first l2))
@@ -18,14 +20,14 @@
 (defn sublist?
   "Returns true if l1 is a sublist of l2, else false."
   [l1 l2]
-  (let [cl1 (count l1)]
-    (if (zero? cl1)
+  (let [l1c (count l1)]
+    (if (zero? l1c)
       true
       (loop [l2 l2
-             cl2 (count l2)]
-        (if (<= cl1 cl2)
-          (or (p-equals? l1 l2)
-              (recur (rest l2) (dec cl2)))
+             l2c (count l2)]
+        (if (<= l1c l2c)
+          (or (sub-equals? l1 l2)
+              (recur (rest l2) (dec l2c)))
           false)))))
 
 (defn classify
