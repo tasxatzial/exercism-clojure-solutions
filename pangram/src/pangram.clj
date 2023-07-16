c143745fcf129d857ccd46157bdea153a13f413a
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-07-16 04:07:14 +0300

    pangram: optimize solution

--- pangram/src/pangram.clj
+++ pangram/src/pangram.clj
@@ -1,22 +1,8 @@
 (ns pangram)
 
-(def alphabet-letter-count 26)
-
-(defn lower-case-letter?
-  "Returns true if character c is a lower-case letter."
-  [c]
-  (<= 97 (int c) 122))
-
-(defn count-different-letters
-  "Returns the number of different letters contained in the given
-  string (ignores case)."
-  [s]
-  (->> s
-       clojure.string/lower-case
-       (filter lower-case-letter?)
+(defn pangram? [sentence]
+  (->> (clojure.string/lower-case sentence)
+       (re-seq #"[a-z]")
        set
-       count))
-
-(defn pangram?
-  [s]
-  (= alphabet-letter-count (count-different-letters s)))
+       count
+       (= 26)))
