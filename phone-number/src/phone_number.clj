fb6ee3f4a26ebfbe6c1c1cb7cc0cccd1cd5b2d11
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-07-23 10:08:04 +0300

    phone number: update exercise & solution

--- phone-number/src/phone_number.clj
+++ phone-number/src/phone_number.clj
@@ -1,25 +1,12 @@
 (ns phone-number)
 
+(def invalid-number "0000000000")
+
 (defn number
-  "Removes any spaces, symbols and the country code from a phone number.
-  If the number is not valid, it returns 0000000000."
   [s]
-  (if (re-matches #"\+?1?[\.|\-| ]?(\([2-9]\d{2}\)|[2-9]\d{2})[\.|\-| ]?[2-9]\d{2}[\.|\-| ]?\d{4}" s)
-    (let [digits (re-seq #"\d" s)]
-      (if (= 11 (count digits))
-        (apply str (rest digits))
-        (apply str digits)))
-    "0000000000"))
-
-(defn area-code
-  "Extracts the area code from a phone number."
-  [s]
-  (-> s
-      number
-      (subs 0 3)))
-
-(defn pretty-print
-  "Formats a phone number as (NXX) NXX-XXXX."
-  [s]
-  (let [n (number s)]
-    (format "(%s) %s-%s" (subs n 0 3) (subs n 3 6) (subs n 6))))
+  (let [s-digits (clojure.string/replace s #"\D" "")]
+    (if (re-matches #"1?[2-9]\d{2}[2-9]\d{6}" s-digits)
+      (if (= 11 (.length s-digits))
+        (subs s-digits 1)
+        s-digits)
+      invalid-number)))
