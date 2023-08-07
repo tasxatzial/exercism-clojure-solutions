545f61df14d73fd722b6f983c0718d0c543c9438
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-08-07 04:07:21 +0300

    allergies: refactoring

--- allergies/src/allergies.clj
+++ allergies/src/allergies.clj
@@ -1,17 +1,14 @@
 (ns allergies)
 
 (def allergens [:eggs :peanuts :shellfish :strawberries :tomatoes :chocolate :pollen :cats])
-(def allergens-power (zipmap allergens (range (count allergens))))
+(def allergen->power (zipmap allergens (range (count allergens))))
 
 (defn allergic-to?
-  "Returns true if the score implies allergy to an allergen, else false."
+  "Returns true if the given score implies allergy to an allergen, else false."
   [score allergen]
-  (->> allergen
-       allergens-power
-       (bit-test score)))
+  (bit-test score (allergen->power allergen)))
 
 (defn allergies
-  "Determines the allergies from the given score. Returns a vector."
+  "Returns all the allergies associated with the given score."
   [score]
-  (keep-indexed #(if (bit-test score %1) %2)
-                allergens))
+  (keep-indexed #(if (bit-test score %1) %2) allergens))
