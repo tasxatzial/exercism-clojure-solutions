d8cd6fc12db30ed722aec438bc1f552d45545d50
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-02-03 01:14:22 +0200

    raindrops: refactoring

--- src/raindrops/src/raindrops.clj
+++ src/raindrops/src/raindrops.clj
@@ -1,21 +1,23 @@
 (ns raindrops)
 
-(def numbers [3 5 7])
-(def pl_ng ["Pling" "Plang" "Plong"])
-(def factor->str (zipmap numbers pl_ng))
+(def factors [3 5 7])
+(def sounds ["Pling" "Plang" "Plong"])
+(def factor->sound (zipmap factors sounds))
 
-(defn num->string
-  "Converts a number to a string. Returns an empty string if
-  3, 5, 7 are not factors of the number."
+(defn number->sounds
+  "Converts a number to a string of its raindrop sounds.
+  Returns an empty string if 3, 5, 7 are not factors of
+  the number."
   [n]
-  (->> numbers
-       (filter #(zero? (mod n %)))
-       (map factor->str)
-       (apply str)))
+  (let [factor-of-n? #(zero? (mod n %))]
+    (->> factors
+         (filter factor-of-n?)
+         (map factor->sound)
+         (apply str))))
 
 (defn convert
   [n]
-  (let [s (num->string n)]
-    (if (= "" s)
+  (let [number-sounds (number->sounds n)]
+    (if (= "" number-sounds)
       (str n)
-      s)))
+      number-sounds)))
