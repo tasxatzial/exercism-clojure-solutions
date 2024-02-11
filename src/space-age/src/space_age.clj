45f2f33d271da1838b1dc194d42169b8b279018f
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-02-11 21:52:39 +0200

    space age: refactor

--- src/space-age/src/space_age.clj
+++ src/space-age/src/space_age.clj
@@ -1,9 +1,8 @@
 (ns space-age)
 
-(def earth-period 31557600)
+(def earth-period-in-secs 31557600)
 
-;; Planet periods relative to earth period
-(def planet-relative-periods
+(def planet->earth-relative-period
   {:earth 1
    :mercury 0.2408467
    :venus 0.61519726
@@ -13,47 +12,44 @@
    :uranus 84.016846
    :neptune 164.79132})
 
-(defn get-planet-period
-  [planet-relative-period]
-  (* earth-period planet-relative-period))
+(def planet->period-in-secs
+  (let [planets (keys planet->earth-relative-period)]
+    (->> planets
+         (map #(* earth-period-in-secs (planet->earth-relative-period %)))
+         (zipmap planets))))
 
-(defn compute-planet-periods
-  []
-  (let [planet-names (keys planet-relative-periods)
-        planet-relative-periods (vals planet-relative-periods)
-        new-planet-periods (map get-planet-period planet-relative-periods)]
-    (zipmap planet-names new-planet-periods)))
-
-(def memoized_compute-planet-periods (memoize compute-planet-periods))
+(defn compute-space-age
+  [planet seconds]
+  (/ seconds (planet->period-in-secs planet)))
 
 (defn on-mercury
   [seconds]
-  (/ seconds (:mercury (memoized_compute-planet-periods))))
+  (compute-space-age :mercury seconds))
 
 (defn on-venus
   [seconds]
-  (/ seconds (:venus (memoized_compute-planet-periods))))
+  (compute-space-age :venus seconds))
 
 (defn on-earth
   [seconds]
-  (/ seconds (:earth (memoized_compute-planet-periods))))
+  (compute-space-age :earth seconds))
 
 (defn on-mars
   [seconds]
-  (/ seconds (:mars (memoized_compute-planet-periods))))
+  (compute-space-age :mars seconds))
 
 (defn on-jupiter
   [seconds]
-  (/ seconds (:jupiter (memoized_compute-planet-periods))))
+  (compute-space-age :jupiter seconds))
 
 (defn on-saturn
   [seconds]
-  (/ seconds (:saturn (memoized_compute-planet-periods))))
+  (compute-space-age :saturn seconds))
 
 (defn on-neptune
   [seconds]
-  (/ seconds (:neptune (memoized_compute-planet-periods))))
+  (compute-space-age :neptune seconds))
 
 (defn on-uranus
   [seconds]
-  (/ seconds (:uranus (memoized_compute-planet-periods))))
+  (compute-space-age :uranus seconds))
