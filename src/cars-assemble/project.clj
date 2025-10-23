418894895e2952120237c25cf7ebbd8483d6d4e0
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-10-23 11:29:38 +0300

    delete learning exercises

--- src/cars-assemble/project.clj
+++
@@ -1,4 +0,0 @@
-(defproject cars-assemble "0.1.0-SNAPSHOT"
-  :description "cars-assemble exercise."
-  :url "https://github.com/exercism/clojure/tree/main/exercises/concept/cars-assemble"
-  :dependencies [[org.clojure/clojure "1.10.0"]])
